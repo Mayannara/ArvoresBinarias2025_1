a9da6b6718c4181830eaf448804047ce0ece1e0e
Matheus Butter <matheusbutterdias@gmail.com>
2025-05-01 17:26:54 -0300

    add altura, quantidade de no e refactory o projeto

--- src/app/Teste.java
+++ src/app/Teste.java
@@ -1,22 +1,26 @@
 package app;
 
-import lib.ArvoreBinariaExemplo;
-import  app.Aluno;
-import lib.IArvoreBinaria;
-import app.ComparadorAlunoPorNome;
+import lib.ArvoreBinaria;
 
 
 public class Teste {
     public static void main(String[] args){
-        ArvoreBinariaExemplo arvore = new ArvoreBinariaExemplo(new ComparadorAlunoPorMatricula());
+        ArvoreBinaria<Aluno> arvore = new ArvoreBinaria<Aluno>(new ComparadorAlunoPorMatricula());
 
-        arvore.adicionar(new Aluno(001,"Maya"));
-        arvore.adicionar(new Aluno(002,"Matheus"));
-        arvore.adicionar(new Aluno(003,"Mariana"));
-        arvore.adicionar(new Aluno(004,"Marina"));
+        arvore.adicionar(new Aluno(100,"Maya"));
+        arvore.adicionar(new Aluno(50,"Matheus"));
+        arvore.adicionar(new Aluno(150,"Mariana"));
+        arvore.adicionar(new Aluno(80,"Marina"));
+        arvore.adicionar(new Aluno(30,"Maria"));
+        arvore.adicionar(new Aluno(120,"Mariana"));
 
-        System.out.println(arvore.pesquisar(new Aluno(003,"")));
+        System.out.println(arvore.pesquisar(new Aluno(100,"")));
 
-        System.out.println(arvore.pesquisar(new Aluno(001,"Marina"),new ComparadorAlunoPorNome()));
+        System.out.println(arvore.pesquisar(new Aluno(150,"Marina"),new ComparadorAlunoPorNome()));
+
+        System.out.println(arvore.altura());
+        System.out.println(arvore.quantidadeNos());
+        System.out.println(arvore.remover(new Aluno(150,"Marina")));
+        System.out.println(arvore.quantidadeNos());
     }
 }
